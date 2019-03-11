aab105d08802145f41822a88435eb21b8d8c211a
Bec <a.becor94@gmail.com>
2019-03-11 17:48:15 -0600

    Get session in view

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -76,124 +76,127 @@ class UsersController extends AppController
      */
     public function login()
     {
-
+        if ($this->Auth->user('id')) { //Check if user is logged in already
+            $this->Flash->success(__('Ya estás logueado'));
+            return $this->redirect(['controller' => 'Users', 'action' => 'index']);
+        }
 
         if ($this->request->is('post')) {
+            //Si el usuario no está autenticado
+            $user = $this->Auth->identify();
 
-            if ($this->Auth->user('id')) { //Check if user is logged in already
-                $this->Flash->success(__('Y estás logueado'));
+            if ($user) {
+                $this->Auth->setUser($user);
+                $this->Flash->success(__('Autenticación aceptada'));
                 return $this->redirect(['controller' => 'Users', 'action' => 'index']);
-            } else {
-                //Si el usuario no está autenticado
-                $user = $this->Auth->identify();
-
-                if ($user) {
-                    $this->Auth->setUser($user);
-                    $this->Flash->success(__('Autenticación aceptada'));
-                    return $this->redirect(['controller' => 'Users', 'action' => 'index']);
-                }
-
-                $this->Flash->error(__('Autenticación erronea'));
-
             }
-        }/*
+
+            $this->Flash->error(__('Autenticación erronea'));
+
+        }
+    /*
         $this->set(['user' => $user]);
         $this->set('_serialize', ['user']);*/
 
-    }
+}
 
-    function logout(){
-        $this->Flash->success(__('Has cerrado sesión'));
-        $this->redirect($this->Auth->logout());
-    }
+function logout()
+{
+    $this->Flash->success(__('Has cerrado sesión'));
+    $this->redirect($this->Auth->logout());
+}
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function signUp()
-    {
+/**
+ * Add method
+ *
+ * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+ */
+public
+function signUp()
+{
 
-        $user = $this->Users->newEntity();
+    $user = $this->Users->newEntity();
 
 //        print_r($this->request->getData());exit;
 //        print_r($user);exit;
-        if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
+    if ($this->request->is('post')) {
+        $user = $this->Users->patchEntity($user, $this->request->getData());
 
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('El usuario ha sido guardado correctamente :D'));
+        if ($this->Users->save($user)) {
+            $this->Flash->success(__('El usuario ha sido guardado correctamente :D'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-
-            /*$query = $this->Users->query();
-            $query->insert(['email', 'password','phone'])
-                ->values([
-                    'email' => $user['email'],
-                    'password' => $user['password'],
-                    'phone' => $user['phone ']
-                ]);
-
-            if ($query->execute()) {
-                $this->Flash->success(__('El usuario ha sido guardado correctamente :D'));
-
-                return $this->redirect(['action' => 'index']);
-            }*/
-
-            $this->Flash->error(__('Imposible guardar. Intente de nuevo.'));
-        }
-        $this->set(compact('user'));
-
-    }
-
-    public function forgotPassword()
-    {
-        // empty for now
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $user = $this->Users->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
-        }
-        $this->set(compact('user'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id User id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $user = $this->Users->get($id);
-        if ($this->Users->delete($user)) {
-            $this->Flash->success(__('The user has been deleted.'));
-        } else {
-            $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+            return $this->redirect(['action' => 'index']);
         }
 
-        return $this->redirect(['action' => 'index']);
+        /*$query = $this->Users->query();
+        $query->insert(['email', 'password','phone'])
+            ->values([
+                'email' => $user['email'],
+                'password' => $user['password'],
+                'phone' => $user['phone ']
+            ]);
+
+        if ($query->execute()) {
+            $this->Flash->success(__('El usuario ha sido guardado correctamente :D'));
+
+            return $this->redirect(['action' => 'index']);
+        }*/
+
+        $this->Flash->error(__('Imposible guardar. Intente de nuevo.'));
     }
+    $this->set(compact('user'));
+
+}
+
+public
+function forgotPassword()
+{
+    // empty for now
+}
+
+/**
+ * Edit method
+ *
+ * @param string|null $id User id.
+ * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+ * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+ */
+public
+function edit($id = null)
+{
+    $user = $this->Users->get($id, [
+        'contain' => []
+    ]);
+    if ($this->request->is(['patch', 'post', 'put'])) {
+        $user = $this->Users->patchEntity($user, $this->request->getData());
+        if ($this->Users->save($user)) {
+            $this->Flash->success(__('The user has been saved.'));
+
+            return $this->redirect(['action' => 'index']);
+        }
+        $this->Flash->error(__('The user could not be saved. Please, try again.'));
+    }
+    $this->set(compact('user'));
+}
+
+/**
+ * Delete method
+ *
+ * @param string|null $id User id.
+ * @return \Cake\Http\Response|null Redirects to index.
+ * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+ */
+public
+function delete($id = null)
+{
+    $this->request->allowMethod(['post', 'delete']);
+    $user = $this->Users->get($id);
+    if ($this->Users->delete($user)) {
+        $this->Flash->success(__('The user has been deleted.'));
+    } else {
+        $this->Flash->error(__('The user could not be deleted. Please, try again.'));
+    }
+
+    return $this->redirect(['action' => 'index']);
+}
 }
--- src/Template/Layout/default.ctp
+++ src/Template/Layout/default.ctp
@@ -25,7 +25,8 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <?= $this->Html->css('base.css') ?>
     <?= $this->Html->css('style.css') ?>
 
@@ -34,29 +35,40 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-bar expanded" data-topbar role="navigation">
-        <ul class="title-area large-3 medium-4 columns">
-            <li class="name">
-                <h1><a href=""><?= $this->fetch('title') ?></a></h1>
-            </li>
-        </ul>
-        <div class="top-bar-section">
-            <ul class="right">
-                <!--<li><a target="_blank" href="https://book.cakephp.org/3.0/">Documentation</a></li>-->
-                <!--<li><a target="_blank" href="https://api.cakephp.org/3.0/">API</a></li>-->
-                <li><?php  echo $this->Html->link('Cerrar Sesión',['controller'=>'users','action'=>'logout']); ?></li>
-            </ul>
-        </div>
-    </nav>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<nav class="top-bar expanded" data-topbar role="navigation">
+    <ul class="title-area large-3 medium-4 columns">
+        <li class="name">
+            <h1><a href=""><?= $this->fetch('title') ?></a></h1>
+        </li>
+    </ul>
+    <div class="top-bar-section">
+        <ul class="right">
+            <!--<li><a target="_blank" href="https://book.cakephp.org/3.0/">Documentation</a></li>-->
+            <!--<li><a target="_blank" href="https://api.cakephp.org/3.0/">API</a></li>-->
+            <?php  if($this->request->getSession()->read('Auth.User.id')){ ?>
 
-    <?= $this->Flash->render() ?>
-    <div class="container clearfix">
-        <?= $this->fetch('content') ?>
+                <li><a style="color: #fff;"><?php echo $this->request->getSession()->read('Auth.User.email') ?> </a> </li>
+                <li><?php  echo $this->Html->link('Cerrar Sesión',['controller'=>'users','action'=>'logout']); ?></li>
+            <?php
+            } ?>
+        </ul>
     </div>
-    <footer>
-    </footer>
-   </body>
+</nav>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+
+<?= $this->Flash->render() ?>
+<div class="container clearfix">
+    <?= $this->fetch('content') ?>
+</div>
+<footer>
+</footer>
+</body>
 </html>
